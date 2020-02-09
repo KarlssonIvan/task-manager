866f3493cb5a00fbdbf4b422668e550c84d2d156
Ivan Karlsson <karlsson@ivans-mbp.mynet>
2020-02-09 22:43:31 +0200

    first endpoint -users was added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,56 +7,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercalse: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-      type: Number,
-      default: 0,
-      validate(value) {
-          if (value < 0) {
-              throw new Error('Age must  be a possitive number')
-          }
-      }
-    }
-});
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
-})
-
-const task = new Task({description:'Kukuriku tarengol', completed: false})
-
-// task.save().then(()=>{
-//     console.log({task})
-// }).catch(error => console.log({error}))
-
-const me = new User({
-  name: "Ivan",
-  email: 'karlsson.ivan@gmail.com'
-});
-
-me.save().then(() => {
-    console.log({me})
-}).catch((error) => {
-    console.log(error)
 })
